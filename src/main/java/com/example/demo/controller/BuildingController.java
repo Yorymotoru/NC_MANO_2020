d93713e796f584d8dac85d8bb363fba48ac45a82
Yorymotoru <sl.borisov.42@outlook.com>
2020-12-07 02:58:05 +0400

    Group into packages updated

---
+++ src/main/java/com/example/demo/controller/BuildingController.java
@@ -0,0 +1,107 @@
+package com.example.demo.controller;
+
+import com.example.demo.DemoApplication;
+import com.example.demo.domain.Building;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping
+public class BuildingController {
+    ArrayList<Building> buildings = new ArrayList<>();
+    private static Logger log = LoggerFactory.getLogger(DemoApplication.class);
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/get")
+    public ResponseEntity getBuilding(@RequestParam(value = "address", defaultValue = "unknown") String address) {
+        Building out = searchBuilding(address);
+        if (out != null) {
+            return new ResponseEntity<>(out, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<Building>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Building> addBuilding(@RequestBody Building building) throws Exception {
+        try {
+            jdbcTemplate.update("INSERT INTO building(address, number_of_floors, residential) VALUES (?, ?, ?)", building.getAddress(),
+                    building.getNumberOfFloors(),
+                    building.getResidential());
+        } catch (Exception JdbcSQLIntegrityConstraintViolationException) {
+            throw new Exception("it already exist");
+        }
+        return new ResponseEntity<>(building, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{address}")
+    public ResponseEntity deleteBuilding(@PathVariable String address) {
+        Building foundBuilding = searchBuilding(address);
+        if (foundBuilding != null) {
+            buildings.remove(foundBuilding);
+            return new ResponseEntity<Building>(HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<Building>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{address}")
+    public ResponseEntity<Building> putBuilding(@PathVariable String address, @RequestBody Building building) {
+        Building foundBuilding = searchBuilding(address);
+        if (foundBuilding != null) {
+            foundBuilding.setNumberOfFloors(building.getNumberOfFloors());
+            foundBuilding.setResidential(building.getResidential());
+            return new ResponseEntity<>(foundBuilding, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PatchMapping("/{address}")
+    public ResponseEntity<Building> patchBuilding(@PathVariable String address, @RequestBody Building building) {
+        Building foundBuilding = searchBuilding(address);
+        if (foundBuilding != null) {
+            if (building.getAddress() != null) {
+                foundBuilding.setAddress(building.getAddress());
+            }
+            if (building.getNumberOfFloors() != 0) {
+                foundBuilding.setNumberOfFloors(building.getNumberOfFloors());
+            }
+            if (building.getResidential() != null) {
+                foundBuilding.setResidential(building.getResidential());
+            }
+            return new ResponseEntity<>(foundBuilding, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    public Building searchBuilding(String address) {
+        Building out = null;
+        for (Building building : buildings) {
+            if (building.getAddress().equals(address)) {
+                out = building;
+            }
+        }
+        return out;
+    }
+
+    public Building searchBuilding(Building newBuilding) {
+        Building out = null;
+        for (Building building : buildings) {
+            if (building.getAddress().equals(newBuilding.getAddress())) {
+                out = building;
+            }
+        }
+        return out;
+    }
+}
---
+++ src/main/java/com/example/demo/domain/Building.java
@@ -0,0 +1,37 @@
+package com.example.demo.domain;
+
+public class Building {
+    private int numberOfFloors;
+    private boolean residential;
+    private String address;
+
+    Building(int numberOfFloors, Boolean residential, String address) {
+        this.numberOfFloors = numberOfFloors;
+        this.residential = residential;
+        this.address = address;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public void setNumberOfFloors(int numberOfFloors) {
+        this.numberOfFloors = numberOfFloors;
+    }
+
+    public Boolean getResidential() {
+        return residential;
+    }
+
+    public void setResidential(Boolean residential) {
+        this.residential = residential;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
